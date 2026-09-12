879fc798b2bd942b3bd2e475d7a07b68d64388f6
HackerOS Team <hackeros068@gmail.com>
2026-09-12 11:21:41 +0200

    Update app.js

--- templates/vanilla/frontend/app.js
+++ templates/vanilla/frontend/app.js
@@ -1,4 +1,5 @@
-// Prawdziwy JS jest dostępny od Silver v0.2 (native/silver_quickjs_shim.c),
+// Prawdziwy JS jest dostępny przez native/silver_quickjs_shim.c
+// (od v0.2 linkowany statycznie, jak reszta shimów Silver),
 // ale trzeba go jawnie włączyć w src/main.h#:
 //
 //   a = app::load_js(a, ["<ścieżka-do-pakietu-silver>/src/js/silver_api.js",
